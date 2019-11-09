6ce36b9b6541f55c4fefe48ca5b43dcd04c22694
1500641 <1500641@edu.karelia.fi>
2019-11-09 20:20:32 +0200

    Readding test project due errors
    Pathing errors and package errors, but lets see the pipeline testing works now.

---
+++ TestAPITests/CustomerTests/CreateCustomer_CorrectDetails_Success.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace TestAPITests
+{
+    [TestClass]
+    public class CreateCustomer_CorrectDetails_Success
+    {
+        [TestMethod]
+        public void CreateCustomer()
+        {
+            Customer customer = new Customer()
+            {
+                CustomerId = 0, //mock
+                FirstName = "Kia",
+                LastName = "Hankola",
+                City = "Helsinki",
+                Address = "Helsingintie 16",
+                DateCreated = DateTime.Today
+            };
+
+            var validationContext = new ValidationContext(customer, null, null);
+            var errors = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(customer, validationContext, errors, true);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
---
+++ TestAPITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAPITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAPITests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("859ba3b2-ebe1-4b40-86ca-6f25b2432e93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

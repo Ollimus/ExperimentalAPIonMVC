5cb90aab03317dd985f13cc4e4735072d073bcc6
Olli Hirvonen <olli.hirvonen@hotmail.fi>
2019-11-27 11:09:55 +0000

    Adding integration tests, new test tool (RestSharper)

---
+++ TestAPITests/IntegrationTests/APIController/CustomersControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using TestAPI.Controllers;
+
+namespace TestAPITests.IntegrationTests.APIController
+{
+    [TestClass]
+    public class CustomersControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsNotNull(baseUrl);
+        }
+    }
+}

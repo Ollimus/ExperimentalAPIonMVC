34ab9076b036b52d16c92e946038065bc4db3236
Olli <1500641@edu.karelia.fi>
2019-12-14 21:41:08 +0200

    Got too excited so forgot to add rest of the files.

---
+++ TestApi.SystemTests/GlobalSetup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestAPI.Models;
+
+namespace TestApi.SystemTests
+{
+    [TestClass]
+    public class GlobalSetup
+    {
+
+
+        [AssemblyInitialize()]
+        public static void SetUp(TestContext context)
+        {
+            var configuration = new TestAPI.Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+
+            SetUpDatabase();
+
+
+        }
+
+        private static void SetUpDatabase()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+
+            _context.Database.ExecuteSqlCommand("TRUNCATE TABLE Customers;");
+            _context.Database.ExecuteSqlCommand("TRUNCATE TABLE Products;");
+            _context.Database.ExecuteSqlCommand("TRUNCATE TABLE Orders;");
+
+
+            //            _context.Database.ExecuteSqlCommand(@"SET IDENTITY_INSERT [dbo].[Customers] ON
+            //INSERT INTO [dbo].[Customers] ([CustomerId], [FirstName], [LastName], [City], [Address], [DateCreated]) VALUES (1, N'Teuvo', N'Hakkarainen', N'', N'', N'1900-01-01 00:00:00')
+            //SET IDENTITY_INSERT [dbo].[Customers] OFF");
+            //            _context.Database.ExecuteSqlCommand(@"SET IDENTITY_INSERT [dbo].[Products] ON
+            //INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Price], [Stock], [Producer]) VALUES (1, N'G15 Keyboard', N'Gaming Keyboard for Gaming uses.', CAST(110.15 AS Decimal(18, 2)), 3, N'Logitech')
+            //SET IDENTITY_INSERT [dbo].[Products] OFF");
+            //            _context.Database.ExecuteSqlCommand(@"SET IDENTITY_INSERT [dbo].[Orders] ON
+            //INSERT INTO [dbo].[Orders] ([Id], [CustomerId], [ProductId], [OrderAmount], [TotalPrice], [TimeAdded]) VALUES (2, 1, 1, 3, CAST(330.45 AS Decimal(18, 2)), N'2019-12-16 00:00:00')
+            //SET IDENTITY_INSERT [dbo].[Orders] OFF");
+
+            _context.SaveChanges();
+            _context.Dispose();
+        }
+    }
+}
---
+++ TestApi.SystemTests/SeleniumTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApi.SystemTests
+{
+    [TestClass]
+    public abstract class SeleniumTest
+    {
+
+        const int iisPort = 2020;
+        private string _applicationName;
+        private Process _iisProcess;
+
+        protected SeleniumTest(string applicationName)
+        {
+            _applicationName = applicationName;
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Start IISExpress
+            StartIIS();
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // Ensure IISExpress is stopped
+            if (_iisProcess.HasExited == false)
+            {
+                _iisProcess.Kill();
+            }
+        }
+
+
+
+        private void StartIIS()
+        {
+            var applicationPath = GetApplicationPath(_applicationName);
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+            _iisProcess = new Process();
+            _iisProcess.StartInfo.ErrorDialog = true;
+            _iisProcess.StartInfo.LoadUserProfile = true;
+            _iisProcess.StartInfo.CreateNoWindow = false;            _iisProcess.StartInfo.UseShellExecute = false;
+            _iisProcess.StartInfo.FileName = programFiles + @"\IIS Express\iisexpress.exe";
+            _iisProcess.StartInfo.Arguments = @"/path:" + applicationPath;
+            _iisProcess.Start();
+        }
+
+
+        protected virtual string GetApplicationPath(string applicationName)
+        {
+            var solutionFolder = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+            return solutionFolder;
+        }
+
+
+        public string GetAbsoluteUrl(string relativeUrl)
+        {
+            if (!relativeUrl.StartsWith("/"))
+            {
+                relativeUrl = "/" + relativeUrl;
+            }
+            return String.Format("http://localhost:{0}{1}", iisPort, relativeUrl);
+        }
+    }
+}
